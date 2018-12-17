31d6f13e157201f77624d000be0f7ddd3787e45e
Philippe Mechai <philippe.mechai@gmail.com>
2018-12-17 21:20:05 +0100

    symbol: implemented all library functions

--- src/symbol.c
+++ src/symbol.c
@@ -22,8 +22,48 @@ scmval intern(scm_ctx_t* ctx, const char* name) {
     return r;
 }
 
+// standard library
+static scmval scm_symbol_p(scm_ctx_t* ctx) {
+    scmval v;
+    v = arg_ref(ctx, 0);
+    if(is_symbol(v))
+        return scm_true;
+    return scm_false;
+}
+
+static scmval scm_symbol_equal_p(scm_ctx_t* ctx) {
+    int argc;
+    scmval *argv;
+    arg_ref_list(ctx, &argc, &argv);
+    scmval s = argv[0];
+    for(int i = 1; i < argc; i++) {
+        if(!is_eq(s, argv[i]))
+            return scm_false;
+    }
+    return scm_true;
+}
+
+static scmval scm_symbol_to_string(scm_ctx_t* ctx) {
+    scmval v;
+    v = arg_ref(ctx, 0);
+    v.type = SCM_TYPE_STRING; // cheapest conversion ever
+    return v;
+}
+
+static scmval scm_string_to_symbol(scm_ctx_t* ctx) {
+    scmval v;
+    v = arg_ref(ctx, 0);
+    v.type = SCM_TYPE_SYMBOL;
+    return v;
+}
+
 // initialization
 void init_symbol(scm_ctx_t* ctx) {
+    define(ctx, "symbol?", scm_symbol_p, arity_exactly(1), 1, any_c);
+    define(ctx, "symbol=?", scm_symbol_equal_p, arity_at_least(2), 1, symbol_c);
+    define(ctx, "symbol->string", scm_symbol_to_string, arity_exactly(1), 1, symbol_c);
+    define(ctx, "string->symbol", scm_string_to_symbol, arity_exactly(1), 1, string_c);
+    
     scm_undef = make_val(SCM_TYPE_UNDEF);
 }
 
