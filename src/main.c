02b41a03b3e1f99d84885d8638708a9552597ace
Philippe Mechai <philippe.mechai@gmail.com>
2018-12-19 12:19:00 +0100

    main: add repl completion for function names

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@ static scm_ctx_t* scm_init() {
     init_errors(ctx);
     init_bool(ctx);
     init_char(ctx);
+    init_symbol(ctx);
     init_pair(ctx);
     init_vector(ctx);
     init_port(ctx);
@@ -28,38 +29,75 @@ static scm_ctx_t* scm_init() {
     return ctx;
 }
 
+//
+// REPL
+//
+static char** proc_names;
+
 static char* get_history_filename() {
     static const char* filename = ".scm_history";
-    char path[4096];
-    snprintf(path, 4096, "%s/%s", getenv("HOME"), filename);
-    return strdup(path);
+    char *path;
+    asprintf(&path, "%s/%s", getenv("HOME"), filename);
+    return path;
 }
 
+static char* proc_name_generator(const char* text, int state) {
+    static int list_index, len;
+    char *name;
+    if(!state) {
+        list_index = 0;
+        len = strlen(text);
+    }
+    while((name = proc_names[list_index++])) {
+        if(!strncmp(name, text, len))
+            return strdup(name);
+    }
+    return NULL;
+}
+
+static char** proc_name_completion_function(const char* text, int start, int end) {
+    rl_attempted_completion_over = 1;
+    return rl_completion_matches(text, proc_name_generator);
+}
+
+static void init_completion(scm_ctx_t* ctx) {
+    scm_fixnum_t size;
+    scmval* procs;
+    dict_keys(ctx->globals, &size, &procs);
+    proc_names = calloc(size+1, sizeof(char*));
+    for(int i = 0; i < size; i++) {
+        proc_names[i] = strdup(string_to_cstr(procs[i]));
+    }
+    proc_names[size] = NULL;
+    rl_attempted_completion_function = proc_name_completion_function;
+}
 
 static void repl(scm_ctx_t* ctx) {
     char* path = get_history_filename();
     char* line = NULL;
     scmval in, v;
     read_history(path);
+    init_completion(ctx);
     while(true) {
         line = readline("> ");
         if(!line)
             break;
         if(*line == ',' && *(line+1) == 'q')
             break;
+        add_history(line);
         try(ctx) {
             in = open_input_string(line);
             v  = read(ctx, in);
             v  = eval(ctx, v, ctx->toplevel);
             if(!is_undef(v)) {
-                write(ctx->current_output_port, v, WRITE_MODE_DISPLAY);
+                write(ctx->current_output_port, v, WRITE_MODE_WRITE);
                 printf("\n");
             }
-            add_history(line);
         } catch {
-            write(ctx->current_error_port, ctx->err, WRITE_MODE_DISPLAY);
+            write(ctx->current_error_port, ctx->err, WRITE_MODE_WRITE);
             fprintf(stderr, "\n");
         }
+        free(line);
     }
     write_history(path);
 }
