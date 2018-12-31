120f5739104da14d8c5629ffa27f7acb75ff43b5
Philippe Mechai <philippe.mechai@gmail.com>
2018-12-31 11:30:34 +0100

    (sarcasm test) track duration and better reporting

--- lib/sarcasm/test.scm
+++ lib/sarcasm/test.scm
@@ -2,40 +2,57 @@
 ;; a simple unit testing framework
 (define-library (sarcasm test)
   (import (scheme base)
-          (scheme write))
-  (export test-begin test-end is TEST-COUNT TEST-FAIL)
+          (scheme write)
+          (scheme time))
+  (export
+    test-begin test-end test-exit is
+    %test-start %test-count %test-fail %do-test)
   (begin
-    (define TEST-COUNT 0)
-    (define TEST-FAIL  0)
+    (define %test-start 0)
+    (define %test-count 0)
+    (define %test-fail  0)
 
     (define (test-begin)
-      (set! TEST-COUNT 0)
-      (set! TEST-FAIL  0))
+      (display (make-string 80 #\-))
+      (newline)
+      (set! %test-start (current-second))
+      (set! %test-count 0)
+      (set! %test-fail  0))
 
     (define (test-end)
-      (display "Tests run:")
-      (display TEST-COUNT)
-      (display " - failed:")
-      (display TEST-FAIL)
+      (display (make-string 80 #\-))
+      (newline)
+      (display "Tests completed in ")
+      (display (exact (truncate (* 1000000 (- (current-second) %test-start)))))
+      (display "ms")
+      (newline)
+      (display "|   total: ") (display %test-count) (display " ") (newline)
+      (display "| success: ") (display (- %test-count %test-fail)) (display " ") (newline)
+      (display "| failure: ") (display %test-fail) (display " ") (newline)
       (newline))
+
+    (define (test-exit)
+      (exit %test-fail))
+
+    (define (%do-test expr expected got)
+      (set! %test-count (+ 1 %test-count))
+      (if (equal? expected got)
+          (begin
+            (display "[PASS] ")
+            (display expr)
+            (newline))
+          (begin
+            (set! %test-fail (+ 1 %test-fail))
+            (display "[FAIL] ")
+            (display expr)
+            (display " => expected:")
+            (display expected)
+            (display " - actual:")
+            (display got)
+            (newline))))
 
     (define-syntax is
       (syntax-rules ()
         ((_ expected expr)
-         (begin
-           (set! TEST-COUNT (+ 1 TEST-COUNT))
-           (if (equal? expected expr)
-             (begin
-               (display "[PASS] ")
-               (display 'expr)
-               (newline))
-             (begin
-               (set! TEST-FAIL (+ 1 TEST-FAIL))
-               (display "[FAIL] ")
-               (display 'expr)
-               (display " => expected:")
-               (display expected)
-               (display " - actual:")
-               (display expr)
-               (newline)))))))))
+         (%do-test 'expr expected expr))))))
 
