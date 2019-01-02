c1293251fa374752d21c888ef0c83610b99bc18b
Philippe Mechai <philippe.mechai@gmail.com>
2019-01-02 10:10:42 +0100

    (scheme cxr) add missing forms

--- lib/scheme/cxr.scm
+++ lib/scheme/cxr.scm
@@ -1,17 +1,17 @@
-#|
-(scheme cxr) library
-|#
+;; (scheme cxr) library
 (define-library (scheme cxr)
-;  (import (scheme base))
   (import (sarcasm core))
-  (export caar cadr cdar cddr
-          caaar caadr cadar cdaar caddr cdadr cddar cdddr
-          cddddr cadddr cdaddr cddadr cdddar caaddr cadadr)                
+  (export 
+    caaar
+    caadr cadar cdaar
+    caddr cdadr cddar
+    cdddr
+    caaaar
+    caaadr caadar cadaar cdaaar 
+    caaddr cadadr cdaadr cdadar cddaar caddar
+    cadddr cdaddr cddadr cdddar
+    cddddr)
   (begin
-    (define (caar lst) (car (car lst)))
-    (define (cadr lst) (car (cdr lst)))
-    (define (cdar lst) (cdr (car lst)))
-    (define (cddr lst) (cdr (cdr lst)))
     (define (caaar lst) (car (car (car lst))))
     (define (caadr lst) (car (car (cdr lst))))
     (define (cadar lst) (car (cdr (car lst))))
@@ -20,11 +20,20 @@
     (define (cdadr lst) (cdr (car (cdr lst))))
     (define (cddar lst) (cdr (cdr (car lst))))
     (define (cdddr lst) (cdr (cdr (cdr lst))))
-    (define (cddddr lst) (cdr (cdr (cdr (cdr lst)))))
+    (define (caaaar lst) (car (car (car (car lst)))))
+    (define (caaadr lst) (car (car (car (cdr lst)))))
+    (define (caadar lst) (car (car (cdr (car lst)))))
+    (define (cadaar lst) (car (cdr (car (car lst)))))
+    (define (cdaaar lst) (cdr (car (car (car lst)))))
+    (define (caaddr lst) (car (car (cdr (cdr lst)))))
+    (define (cadadr lst) (car (cdr (car (cdr lst)))))
+    (define (cdaadr lst) (cdr (car (car (cdr lst)))))
+    (define (cdadar lst) (cdr (car (cdr (car lst)))))
+    (define (cddaar lst) (cdr (cdr (car (car lst)))))
+    (define (caddar lst) (car (cdr (cdr (car lst)))))
     (define (cadddr lst) (car (cdr (cdr (cdr lst)))))
     (define (cdaddr lst) (cdr (car (cdr (cdr lst)))))
     (define (cddadr lst) (cdr (cdr (car (cdr lst)))))
     (define (cdddar lst) (cdr (cdr (cdr (car lst)))))
-    (define (caaddr lst) (car (car (cdr (cdr lst)))))
-    (define (cadadr lst) (car (cdr (car (cdr lst)))))))
+    (define (cddddr lst) (cdr (cdr (cdr (cdr lst))))))
 
