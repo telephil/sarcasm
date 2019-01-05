d79b012347c876243a2adced755b9d945a13d920
Philippe Mechai <philippe.mechai@gmail.com>
2019-01-05 20:35:17 +0100

    number: all number procedures now handle bignums

--- src/number.c
+++ src/number.c
@@ -11,6 +11,7 @@ scmval scm_nan;
 ////////////////////////////////////////////////////////////////////////////////
 // forward declarations
 ////////////////////////////////////////////////////////////////////////////////
+static int promote(scmval*, scmval*);
 static int ncmp(scmval, scmval);
 static scmval nadd(scmval, scmval);
 static scmval nmul(scmval, scmval);
@@ -129,25 +130,36 @@ static scmval scm_zero_p(scmval x) {
 
 static scmval scm_positive_p(scmval x) {
     check_arg("positive?", number_c, x);
-    if(is_pos_inf(x))       return scm_true;
-    else if(is_neg_inf(x))  return scm_false;
-    else if(is_nan(x))      return scm_false;
-    return scm_bool((is_fixnum(x) && c_fix(x) >= 0) || (is_flonum(x) && c_flo(x) >= 0.0));
+    bool ret = false;
+    if(is_pos_inf(x))       ret = true;
+    else if(is_neg_inf(x))  ret = false;
+    else if(is_nan(x))      ret = false;
+    else if(is_fixnum(x))   ret = c_fix(x) >= 0;
+    else if(is_flonum(x))   ret = c_flo(x) >= 0.0;
+    else if(is_bignum(x))   ret = mpz_sgn(c_big(x)) > 0;
+    return scm_bool(ret);
 }
 
 static scmval scm_negative_p(scmval x) {
     check_arg("negative?", number_c, x);
-    if(is_pos_inf(x))       return scm_false;
-    else if(is_neg_inf(x))  return scm_true;
-    else if(is_nan(x))      return scm_false;
-    return scm_bool((is_fixnum(x) && c_fix(x) < 0) || (is_flonum(x) && c_flo(x) < 0.0));
+    bool ret = false;
+    if(is_pos_inf(x))       ret = false;
+    else if(is_neg_inf(x))  ret = true;
+    else if(is_nan(x))      ret = false;
+    else if(is_fixnum(x))   ret = c_fix(x) < 0;
+    else if(is_flonum(x))   ret = c_flo(x) < 0.0;
+    else if(is_bignum(x))   ret = mpz_sgn(c_big(x)) < 0;
+    return scm_bool(ret);
 }
 
 static scmval scm_odd_p(scmval x) {
     check_arg("odd?", number_c, x);
-    if(!is_fixnum(x))
+    if(!is_integer(x))
         return scm_true;
-    return scm_bool(c_fix(x) & 1);
+    bool ret = false;
+    if(is_fixnum(x))        ret = c_fix(x) & 1;
+    else if(is_bignum(x))   ret = mpz_odd_p(c_big(x));
+    return scm_bool(ret);
 }
 
 static scmval scm_even_p(scmval x) {
@@ -210,6 +222,7 @@ static scmval scm_div(int argc, scmval* argv) {
     if(argc == 1) return ndiv(scm_fix(1), argv[0]);
     scmval x = argv[0];
     for(int i = 1; i < argc; i++) {
+        if(is_true(scm_zero_p(argv[i]))) error(scm_undef, "division by 0 is not defined");
         x = ndiv(x, argv[i]);
     }
     return x;
@@ -224,6 +237,13 @@ static scmval scm_abs(scmval x) {
         fixnum i = c_fix(x);
         if(i >= 0) return x;
         return scm_fix(-i);
+    } else if(is_bignum(x)) {
+        mpz_t r;
+        mpz_init(r);
+        mpz_abs(r, c_big(x));
+        mpz_clear(c_big(x));
+        scm_delete(x.o);
+        return scm_big(r);
     } else {
         flonum f = c_flo(x);
         if(f >= 0) return x;
@@ -232,33 +252,110 @@ static scmval scm_abs(scmval x) {
     return scm_undef; // not reached
 }
 
+static inline scmval big_or_fix(mpz_t n) {
+    if(mpz_fits_slong_p(n))
+        return scm_fix(mpz_get_si(n));
+    return scm_big(n);
+}
+
 static scmval scm_quotient(scmval n1, scmval n2) {
     check_arg("quotient", integer_c, n1);
     check_arg("quotient", integer_c, n2);
-    fixnum i1 = c_fix(n1);
-    fixnum i2 = c_fix(n2);
-    ldiv_t d = ldiv(i1, i2);
-    return scm_fix(d.quot);
+    scmval r = scm_0;
+    if(is_fixnum(n1)) {
+        if(is_fixnum(n2)) {
+            ldiv_t d = ldiv(c_fix(n1), c_fix(n2));
+            r = scm_fix(d.quot);
+        } else if(is_bignum(n2)) {
+            scmval b = scm_bigi(c_fix(n1));
+            mpz_t q;
+            mpz_init(q);
+            mpz_tdiv_q(q, c_big(b), c_big(n2));
+            r = big_or_fix(q);
+        }
+    } else if(is_bignum(n1)) {
+        if(is_fixnum(n2)) {
+            scmval b = scm_bigi(c_fix(n2));
+            mpz_t q;
+            mpz_init(q);
+            mpz_tdiv_q(q, c_big(n1), c_big(b));
+            r = big_or_fix(q);
+        } else if(is_bignum(n2)) {
+            mpz_t q;
+            mpz_init(q);
+            mpz_tdiv_q(q, c_big(n1), c_big(n2));
+            r = big_or_fix(q);
+        }
+    }
+    return r;
 }
 
 static scmval scm_remainder(scmval n1, scmval n2) {
     check_arg("remainder", integer_c, n1);
     check_arg("remainder", integer_c, n2);
-    fixnum i1 = c_fix(n1);
-    fixnum i2 = c_fix(n2);
-    ldiv_t d = ldiv(i1, i2);
-    return scm_fix(d.rem);
+    scmval r = scm_0;
+    if(is_fixnum(n1)) {
+        if(is_fixnum(n2)) {
+            ldiv_t d = ldiv(c_fix(n1), c_fix(n2));
+            r = scm_fix(d.quot);
+        } else if(is_bignum(n2)) {
+            scmval b = scm_bigi(c_fix(n1));
+            mpz_t q;
+            mpz_init(q);
+            mpz_tdiv_r(q, c_big(b), c_big(n2));
+            r = big_or_fix(q);
+        }
+    } else if(is_bignum(n1)) {
+        if(is_fixnum(n2)) {
+            scmval b = scm_bigi(c_fix(n2));
+            mpz_t q;
+            mpz_init(q);
+            mpz_tdiv_r(q, c_big(n1), c_big(b));
+            r = big_or_fix(q);
+        } else if(is_bignum(n2)) {
+            mpz_t q;
+            mpz_init(q);
+            mpz_tdiv_r(q, c_big(n1), c_big(n2));
+            r = big_or_fix(q);
+        }
+    }
+    return r;
 }
 
 static scmval scm_modulo(scmval n1, scmval n2) {
     check_arg("remainder", integer_c, n1);
     check_arg("remainder", integer_c, n2);
-    fixnum i1 = c_fix(n1);
-    fixnum i2 = c_fix(n2);
-    fixnum m  = i1 % i2;
-    if(m < 0)
-        m = (i2 < 0) ? m - i2 : m + i2;
-    return scm_fix(m);
+    scmval r = scm_0;
+    if(is_fixnum(n1)) {
+        if(is_fixnum(n2)) {
+            fixnum i1 = c_fix(n1);
+            fixnum i2 = c_fix(n2);
+            fixnum m  = i1 % i2;
+            if(m < 0)
+                m = (i2 < 0) ? m - i2 : m + i2;
+            r = scm_fix(m);
+        } else if(is_bignum(n2)) {
+            scmval b = scm_bigi(c_fix(n1));
+            mpz_t m;
+            mpz_init(m);
+            mpz_mod(m, c_big(b), c_big(n2));
+            r = big_or_fix(m);
+        }
+    } else if(is_bignum(n1)) {
+        if(is_fixnum(n2)) {
+            scmval b = scm_bigi(c_fix(n2));
+            mpz_t m;
+            mpz_init(m);
+            mpz_mod(m, c_big(n1), c_big(b));
+            r = big_or_fix(m);
+        } else if(is_bignum(n2)) {
+            mpz_t m;
+            mpz_init(m);
+            mpz_mod(m, c_big(n1), c_big(n2));
+            r = big_or_fix(m);
+        }
+    }
+    return r;
 }
 
 static scmval scm_numerator(scmval q) {
@@ -308,7 +405,11 @@ static scmval scm_round(scmval x) {
     return scm_flo(round(c_flo(x)));
 }
 
-static inline flonum inexact(scmval z) { return is_fixnum(z) ? (flonum)c_fix(z) : c_flo(z); }
+static inline flonum inexact(scmval z) { 
+    if(is_fixnum(z))        return (flonum)c_fix(z);
+    else if(is_bignum(z))   return mpz_get_d(c_big(z));
+    else                    return c_flo(z);
+}
 
 static scmval scm_exp(scmval z) {
     check_arg("exp", number_c, z);
@@ -367,35 +468,60 @@ static scmval scm_square(scmval z) {
 static scmval scm_sqrt(scmval z) {
     check_arg("sqrt", number_c, z);
     if(ncmp(z, scm_0) < 0) error(scm_undef, "sqrt is not defined for negative numbers");
-    scmval res;
-    if(is_fixnum(z))
+    scmval res = scm_nan;
+    if(is_fixnum(z)) {
         res = scm_flo(sqrt(c_fix(z)));
-    else
+    } else if(is_bignum(z)) {
+        mpz_t s;
+        mpz_init(s);
+        mpz_sqrt(s, c_big(z));
+        res = big_or_fix(s);
+    } else if(is_flonum(z)) {
         res = scm_flo(sqrt(c_flo(z)));
+    }
     return res;
 }
 
 static scmval scm_expt(scmval z1, scmval z2) {
     check_arg("expt", number_c, z1);
     check_arg("expt", number_c, z2);
-    flonum f1 = inexact(z1);
-    flonum f2 = inexact(z2);
-    flonum f3 = pow(f1, f2);
-    if(is_fixnum(z1) && is_fixnum(z2))
-        return scm_fix(f3);
-    return scm_flo(f3);
+    scmval ret = scm_nan;
+    int type = promote(&z1, &z2);
+    switch(type) {
+    case SCM_TYPE_FLONUM:
+        {
+            flonum f1 = c_flo(z1);
+            flonum f2 = c_flo(z2);
+            ret = scm_flo(pow(f1, f2));
+            break;
+        }
+    case SCM_TYPE_FIXNUM:
+    case SCM_TYPE_BIGNUM:
+        {
+            mpz_t p;
+            mpz_init(p);
+            mpz_pow_ui(p, c_big(z1), mpz_get_ui(c_big(z2)));
+            ret = big_or_fix(p);
+            break;
+        }
+    }
+    return ret;
 }
 
 static scmval scm_inexact(scmval x) {
     check_arg("inexact", number_c, x);
-    if(is_flonum(x))
-        return x;
-    return scm_flo((flonum)c_fix(x));
+    scmval ret = x;
+    if(is_fixnum(x)) {
+        ret = scm_flo((flonum)c_fix(x));
+    } else if(is_bignum(x)) {
+        ret = scm_flo(mpz_get_d(c_big(x)));
+    }
+    return ret;
 }
 
 static scmval scm_exact(scmval n) {
     check_arg("exact", number_c, n);
-    if(is_fixnum(n))
+    if(is_fixnum(n) || is_bignum(n))
         return n;
     return scm_fix((fixnum)c_flo(n));
 }
@@ -414,6 +540,9 @@ static scmval scm_number_to_string(scmval z, scmval radix) {
     check_arg("number->string", radix_c, radix);
     if(is_flonum(z))
         return scm_to_string(z);
+    else if(is_bignum(z))
+        return scm_str(mpz_get_str(NULL, c_fix(radix), c_big(z)));
+
     scmval result;
     char *buf;
     fixnum i = c_fix(z);
@@ -562,6 +691,7 @@ scmval string_to_number(char* buf) {
         }
         if(neg) {
             mpz_t n;
+            mpz_init(n);
             mpz_neg(n, m);
             mpz_clear(m);
             return scm_big(n);
@@ -593,6 +723,28 @@ static int ncmp(scmval x, scmval y) {
             if(diff < 0)        res = -1;
             else if(diff > 0)   res = 1;
             else                res = 0;
+        } else if(is_bignum(y)) {
+            int cmp = mpz_cmp_si(c_big(y), c_fix(x));
+            if(cmp > 0)         res = -1;
+            else if(cmp < 0)    res = 1;
+            else                res = 0;
+        }
+    } else if(is_bignum(x)) {
+        if(is_fixnum(y)) {
+            int cmp = mpz_cmp_si(c_big(x), c_fix(y));
+            if(cmp < 0)         res = -1;
+            else if(cmp > 0)    res = 1;
+            else                res = 0;
+        } else if(is_flonum(y)) {
+            int cmp = mpz_cmp_d(c_big(x), c_flo(y));
+            if(cmp < 0)         res = -1;
+            else if(cmp > 0)    res = 1;
+            else                res = 0;
+        } else if(is_bignum(y)) {
+            int cmp = mpz_cmp(c_big(x), c_big(y));
+            if(cmp < 0)         res = -1;
+            else if(cmp > 0)    res = 1;
+            else                res = 0;
         }
     } else if(is_flonum(x)) {
         if(is_fixnum(y)) {
@@ -602,7 +754,11 @@ static int ncmp(scmval x, scmval y) {
             if(diff < 0)        res = -1;
             else if(diff > 0)   res = 1;
             else                res = 0;
-            
+        } else if(is_bignum(y)) {
+            int cmp = mpz_cmp_d(c_big(y), c_fix(x));
+            if(cmp > 0)         res = -1;
+            else if(cmp < 0)    res = 1;
+            else                res = 0;
         }
     }
     return res;
@@ -655,10 +811,7 @@ static scmval nadd(scmval x, scmval y) {
                 mpz_t m;
                 mpz_init(m);
                 mpz_add(m, c_big(x), c_big(y));
-                if(mpz_fits_slong_p(m))
-                    r = scm_fix(mpz_get_si(m));
-                else
-                    r = scm_big(m);
+                r = big_or_fix(m);
                 mpz_clear(m);
             }
             break;
@@ -679,10 +832,7 @@ static scmval nmul(scmval x, scmval y) {
                 mpz_t m;
                 mpz_init(m);
                 mpz_mul(m, c_big(x), c_big(y));
-                if(mpz_fits_slong_p(m))
-                    r = scm_fix(mpz_get_si(m));
-                else
-                    r = scm_big(m);
+                r = big_or_fix(m);
                 mpz_clear(m);
             }
             break;
@@ -703,10 +853,7 @@ static scmval nsub(scmval x, scmval y) {
                 mpz_t m;
                 mpz_init(m);
                 mpz_sub(m, c_big(x), c_big(y));
-                if(mpz_fits_slong_p(m))
-                    r = scm_fix(mpz_get_si(m));
-                else
-                    r = scm_big(m);
+                r = big_or_fix(m);
                 mpz_clear(m);
             }
             break;
@@ -727,10 +874,7 @@ static scmval ndiv(scmval x, scmval y) {
                 mpz_t m;
                 mpz_init(m);
                 mpz_div(m, c_big(x), c_big(y));
-                if(mpz_fits_slong_p(m))
-                    r = scm_fix(mpz_get_si(m));
-                else
-                    r = scm_big(m);
+                r = big_or_fix(m);
                 mpz_clear(m);
             }
             break;
