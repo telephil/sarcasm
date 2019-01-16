17ebc1350a18eb1ed267d11b14d5df904bfeaa99
Philippe Mechai <philippe.mechai@gmail.com>
2019-01-16 10:29:16 +0100

    (sarcasm repl) improved read and print loop
    read loop has been rewritten to give user a chance to complete an unfinished sequence after a newline
    print has been changed to avoid printing void values

--- lib/sarcasm/repl.scm
+++ lib/sarcasm/repl.scm
@@ -38,7 +38,7 @@
       (save-history)
       (display "Bye\n"))
 
-    (define (error-handler err)
+    (define (print-error err)
       (display "\x001b;[31mERROR\x001b;[0m ")
       (if (error-object? err)
           (begin
@@ -59,21 +59,45 @@
             (write err)
             (newline)))) 
 
+    (define (print obj)
+      (unless (void? obj)
+        (write obj)))
+
+    ;; filter EOF errors while reading
+    ;; to allow user to complete expression
+    (define (skip-error? err)
+      (and (error-object? err)
+           (starts-with? (error-object-message err)
+                         "unexpected end of file")))
+
     (define (repl)
       (register-exit-hook end)
       (banner)
       (load-history)
       (set-completion-function! completions-for)
-      (let loop ((line (readline "> ")))
-        (when line
-          (add-history line)
-          (with-exception-handler
-            error-handler
-            (lambda ()
-              (let* ((r (read-from-string line))
-                     (v (eval r (interaction-environment))))
-                (unless (eof-object? v)
-                  (write v)
-                  (newline)))))
-          (loop (readline "> ")))))
+      (let loop ((buffer ""))
+        (let ((line (readline (if (zero? (string-length buffer)) "> " "+ "))))
+          (if (not line) ;; empty string -> done
+              (newline)
+              (let ((input (string-append buffer line "\n")))
+                (add-history input)
+                (call-with-current-continuation
+                  (lambda (return)
+                    (with-exception-handler
+                      (lambda (err)
+                        (unless (skip-error? err)
+                          (print-error err)
+                          (set! input ""))
+                        (return #f))
+                      (lambda ()
+                        (call-with-port
+                          (open-input-string input)
+                          (lambda (port)
+                           (let read-loop ((expr (read port)))
+                             (unless (eof-object? expr)
+                               (print (eval expr (interaction-environment)))
+                               (newline)
+                               (set! input "")
+                               (read-loop (read port))))))))))
+                (loop input))))))
     ))
