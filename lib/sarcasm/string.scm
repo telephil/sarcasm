19e080c45cc6c76472ea518751df2d24c6958a96
Philippe Mechai <philippe.mechai@gmail.com>
2019-01-16 10:52:44 +0100

    (sarcasm string) non-standard string procedures

---
+++ lib/sarcasm/string.scm
@@ -0,0 +1,17 @@
+(define-library (sarcasm string)
+  (import (sarcasm core))
+  (export string-empty? starts-with?)
+  (begin
+
+    ;; check whether string <str> is empty
+    (define (string-empty? str)
+      (string=? str ""))
+
+    ;; check whether string <s> starts with <t>
+    (define (starts-with? s t)
+      (define lt (string-length t))
+      (define ls (string-length s))
+      (or (zero? lt)
+          (and (<= lt ls)
+               (string=? (substring s 0 (- lt 1)) t))))
+    ))
